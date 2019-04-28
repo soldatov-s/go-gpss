4ecdbfcfbcd5a76728814046dde5bfd842a9e72c
Sergey Soldatov <ssacompany@mail.ru>
2019-04-28 21:29:39 +0300

    added count object

---
+++ count.go
@@ -0,0 +1,47 @@
+// Copyright 2019 Sergey Soldatov. All rights reserved.
+// This software may be modified and distributed under the terms
+// of the Apache license. See the LICENSE file for details.
+
+package gpss
+
+import (
+	"fmt"
+)
+
+type Count struct {
+	BaseObj
+	value   *int
+	inc_dec int
+}
+
+// Creates two objects, for incremet and decrement. After enter transact in
+// inc_obj, value incremented by inc_value. After enter transact in dec_obj,
+// value decremented by dec_value.
+func NewCount(name string, inc_value, dec_value int) (*Count, *Count) {
+	value := 0
+	inc := &Count{}
+	dec := &Count{}
+	inc.name = name + "_INC"
+	inc.value = &value
+	inc.inc_dec = inc_value
+	dec.name = name + "_DEC"
+	dec.value = inc.value
+	dec.inc_dec = dec_value
+	return inc, dec
+}
+
+func (obj *Count) AppendTransact(transact ITransaction) bool {
+	transact.PrintInfo()
+	for _, v := range obj.GetDst() {
+		if v.AppendTransact(transact) {
+			*obj.value += obj.inc_dec
+			obj.GetLogger().GetTrace().Println("Append transact ", transact.GetId(), " to Count")
+			return true
+		}
+	}
+	return false
+}
+
+func (obj *Count) PrintReport() {
+	fmt.Printf("Count value %d\n", obj.value)
+}
