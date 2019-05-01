bd7a86e4841d7972bc73ac22118ef417e67a3721
Sergey Soldatov <ssacompany@mail.ru>
2019-05-01 16:14:05 +0300

    doc

--- check.go
+++ check.go
@@ -4,11 +4,21 @@
 
 package gpss
 
+// Check is object for check parameters of transact or any another parameters
+// sumulation model. Check maybe have two results: true ir false. You can replace
+// Checking Handler with your handler.
 type Check struct {
 	BaseObj
+	// Checking handler
 	HandleChecking HandleCheckingFunc
-	falseObj       IBaseObj
-	parameters     []Parameter
+	// Destination object in case false result of checking
+	falseObj IBaseObj
+	// Parameters of transact for checking
+	parameters []Parameter
+	// For counting true result checking
+	cnt_true int
+	// For counting false result checking
+	cnt_false int
 }
 
 type HandleCheckingFunc func(obj *Check, transact ITransaction) bool
@@ -26,6 +36,7 @@ func Checking(obj *Check, transact ITransaction) bool {
 
 func NewCheck(name string, hndl HandleCheckingFunc, falseObj IBaseObj, parameters ...Parameter) *Check {
 	obj := &Check{parameters: parameters, falseObj: falseObj}
+	obj.name = name
 	if hndl != nil {
 		obj.HandleChecking = hndl
 	} else {
@@ -40,17 +51,21 @@ func (obj *Check) AppendTransact(transact ITransaction) bool {
 	if !obj.HandleChecking(obj, transact) {
 		if obj.falseObj != nil {
 			if obj.falseObj.AppendTransact(transact) {
+				obj.cnt_true++
 				return true
 			} else {
+				obj.cnt_false++
 				return false
 			}
 		}
 	}
 	for _, v := range obj.GetDst() {
 		if v.AppendTransact(transact) {
+			obj.cnt_true++
 			return true
 		}
 	}
+	obj.cnt_false++
 	return false
 }
 
